968266da013956bf3882f59e061d2f6bf6e18f51
Hydrangeaze <hydrangeaze@proton.me>
2024-08-28 16:28:01 +0300

    flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     home-manager = {
        url = "github:nix-community/home-manager";
        inputs.nixpkgs.follows = "nixpkgs";
